2a36e4ed0f212f8e84993c70279a7734fe038ad2
readyyyk <mr.sairexer@gmail.com>
2024-07-04 21:25:54 +0300

    useParams works fine now...

--- test/create-app.tsx
+++ test/create-app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { CartApi, ExampleApi } from "../src/client/api";
 import { initStore } from "../src/client/store";
-import { MemoryRouter, Route } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Application } from "../src/client/Application";
 import { BASE_NAME } from "./unit/config";
